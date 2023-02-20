1079e28c5b042690af2676348b7a51a793148ac7
hyungjuIM <hyungju9753@gmail.com>
2023-02-20 19:16:14 +0900

    첫 번째 커밋 생성.

---
+++ src/edu/kh/hit/practice/GitPracticeRun.java
@@ -0,0 +1,10 @@
+package edu.kh.hit.practice;
+
+public class GitPracticeRun {
+
+	public static void main(String[] args) {
+System.out.println("안녕하세요~!");
+System.out.println("Git연습용 파일입니다");
+	}
+
+}
