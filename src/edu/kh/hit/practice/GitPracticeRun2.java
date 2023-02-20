3e34af10f5affce888dfa18a688b2d57aaf31f06
hyungjuIM <hyungju9753@gmail.com>
2023-02-20 20:15:29 +0900

    최종 커밋

--- src/edu/kh/hit/practice/GitPracticeRun2.java
+++ src/edu/kh/hit/practice/GitPracticeRun2.java
@@ -3,8 +3,8 @@ package edu.kh.hit.practice;
 public class GitPracticeRun2 {
 
 	public static void main(String[] args) {
-System.out.println("두 번째 git 생성");
-System.out.println("asdasdasd");
+		System.out.println("두 번째 git 생성");
+		System.out.println("asdasdasd");
 	}
 
 }
