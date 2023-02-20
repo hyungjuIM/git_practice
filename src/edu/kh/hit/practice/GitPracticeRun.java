d2fd877d1234fc74af3105ad688868befdbd74be
hyungjuIM <hyungju9753@gmail.com>
2023-02-20 19:20:11 +0900

    두번 째 주석 삭제

--- src/edu/kh/hit/practice/GitPracticeRun.java
+++ src/edu/kh/hit/practice/GitPracticeRun.java
@@ -3,8 +3,8 @@ package edu.kh.hit.practice;
 public class GitPracticeRun {
 
 	public static void main(String[] args) {
-System.out.println("안녕하세요~!");
-System.out.println("Git연습용 파일입니다");
+		System.out.println("안녕하세요~!");
+	
 	}
 
 }
